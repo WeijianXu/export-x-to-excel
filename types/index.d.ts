606733af63299dced5a2b16a2241cc7b3e809d3a
xuweijian <weijian.xuwj@dtzhejiang.com>
2022-04-26 18:34:28 +0800

    fix: 修复类型定义

--- types/index.d.ts
+++ types/index.d.ts
@@ -8,7 +8,7 @@ export type Range = XlsxAange;
  * @param jsonData 表格主体数据
  * @param title 表格名称
  */
-export declare function export_data_to_excel(th: any[], jsonData: any[], title?: string): void;
+export declare type export_data_to_excel = (th: any[], jsonData: any[], title?: string) => void;
 
 /**
  * 将 `generate_table_array` 获取的数据传入；适用于拼接多个 table
@@ -16,10 +16,10 @@ export declare function export_data_to_excel(th: any[], jsonData: any[], title?:
  * @param tableArray 使用 `generate_table_array` 方法生成的数据列表
  * @param title 表格名称
  */
-export declare function export_table_array_to_excel(
+export declare type export_table_array_to_excel = (
   tableArray: [any[], Range[]],
   title?: string,
-): void;
+) => void;
 
 /**
  * 导出指定 id 的表格
@@ -27,11 +27,11 @@ export declare function export_table_array_to_excel(
  * @param id 表格元素 Table 所使用的 id 属性值
  * @param title 表格名称
  */
-export declare function export_table_to_excel(id: string, title?: string): void;
+export declare type export_table_to_excel = (id: string, title?: string) => void;
 
 /**
  * 根据表格元素Table生成导出Excel所需的数据对象
  * @param table 表格元素对象
  * @returns 导出Excel所需的数据对象
  */
-export default function generate_table_array(table?: HTMLElement | null): [any[], Range[]];
+export declare type generate_table_array = (table?: HTMLElement | null) => [any[], Range[]];
